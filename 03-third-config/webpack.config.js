20f62679a4063f349a2efd00ab4e9113fbce0868
junqiang.shen <junqiang.shen@qunar.com>
2016-12-24 23:10:52 +0800

    03 third config step 2

--- 03-third-config/webpack.config.js
+++ 03-third-config/webpack.config.js
@@ -10,5 +10,9 @@ module.exports = {
             test: /\.css$/,
             loader: "style!css"
         }]
-    }
+    },
+    plugins: [
+        // 注意插件可能不止一个，也是个数组,Webpack就是最上面引入的变量
+        new Webpack.BannerPlugin("打包文件头部测试\nFBI Warning!!!")
+    ]
 }
